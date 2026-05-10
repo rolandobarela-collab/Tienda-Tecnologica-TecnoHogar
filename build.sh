55acbcc56621642502c3d723646ec76e01fe411a
Rolando Barela <rolandobarela@gmail.com>
2026-05-10 00:53:11 -0500

    fix: build.sh actualizado

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -o errexit
 
-pip install -r tecnohogar/requirements.txt
+pip install -r requirements.txt
 python tecnohogar/manage.py collectstatic --no-input
 python tecnohogar/manage.py migrate
