37ff26121c97930a18eb205eb55ffe4f44745986
Rolando Barela <rolandobarela@gmail.com>
2026-05-10 02:09:22 -0500

    fix: collectstatic en ruta correcta

--- build.sh
+++ build.sh
@@ -2,5 +2,6 @@
 set -o errexit
 
 pip install -r requirements.txt
-python tecnohogar/manage.py collectstatic --no-input
-python tecnohogar/manage.py migrate
+cd tecnohogar
+python manage.py collectstatic --no-input
+python manage.py migrate
