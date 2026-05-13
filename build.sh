f0898a247aa71f74ad94a1e7c55a3786facedbd8
Rolando Barela <rolandobarela@gmail.com>
2026-05-13 15:28:17 -0500

    fix: crear superusuario automatico y PostgreSQL

--- build.sh
+++ build.sh
@@ -5,3 +5,4 @@ pip install -r requirements.txt
 cd tecnohogar
 python manage.py collectstatic --no-input
 python manage.py migrate
+echo "from django.contrib.auth.models import User; User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@tecnohogar.com', 'admin123')" | python manage.py shell
